c2ee048a6315212a3d347ba8470e99f581a55a4d
Vaheeth <vaheethraj250@gmail.com>
2025-05-02 23:59:52 +0530

    Infra ready code

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "test" {
-  ami = "ami-084568db4383264d4"
-  instance_type = "t2.micro"
+  ami = var.ami
+  instance_type = var.instance_type["instance_type"]
 }
--- variables.tf
+++ variables.tf
@@ -4,3 +4,11 @@ variable "region" {
   description = "Test"
 }
 
+variable "instance_type" {
+  default = {
+    "instance_type" = "t2.micro"
+    "instance_types" = "t3.micro"
+  }
+  type = map(string)
+  description = "Instance Type"
+}
